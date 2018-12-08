4a8e7e99fd253bc65ff6942f415ff088a687b888
JaredWright <jared.wright12@gmail.com>
2018-12-08 01:56:52 +0000

    Progress toward interrupt coordination between NDVM and host

--- bfvmm/src/hve/arch/intel_x64/vtd/interrupt_remapping.cpp
+++ bfvmm/src/hve/arch/intel_x64/vtd/interrupt_remapping.cpp
@@ -12,34 +12,61 @@ namespace interrupt_remapping
 {
 
 intel_x64::vtd::imap g_vtd_imap{};
-bfn::once_flag init_flag{};
-bfn::once_flag fini_flag{};
+bfn::once_flag turn_on_remapping_flag{};
 
 using namespace eapis::intel_x64;
 namespace vtd = intel_x64::vtd;
 
 // The two interrupt vectors to be remapped to/from each other
 static volatile uint64_t g_visr_vector = 0;
-static volatile uint64_t g_ndvm_vector = 0;
+static volatile uint64_t g_ndvm_vector = 1;
 
 // The physical bus/device/function the emulated device will occupy
-const uint64_t g_bus = 2;
-const uint64_t g_device = 0;
-const uint64_t g_function = 0;
+uint64_t g_bus = 0;
+uint64_t g_device = 0;
+uint64_t g_function = 0;
 
-bool
-interrupt_vector_cpuid(
-    gsl::not_null<vcpu_t *> vcpu,
-    cpuid_handler::info_t &info
-)
+void
+dump_interrupt_remapping()
 {
-    bfignored(vcpu);
-    bfignored(info);
+    bfdebug_info(0, "-------------------------------------------");
+    bfdebug_info(0, "      VT-d Interrupt Remapping Status");
+    bfdebug_info(0, "-------------------------------------------");
 
-    g_visr_vector = vcpu->rcx();
-    // bfdebug_nhex(0, "Initializing interrupt remapping for vector:", g_visr_vector);
+    auto irta = vtd::iommu::irta_reg::get();
+    vtd::iommu::irta_reg::dump(0, irta);
 
-    bfn::call_once(init_flag, [&] {
+    intel_x64::vtd::iommu::gsts_reg::dump(0);
+    auto irta_size = (2ul << (vtd::iommu::irta_reg::s::get(irta) + 1));
+    size_t irt_entry_count = irta_size / sizeof(vtd::irte::value_type);
+    bfdebug_subnhex(0, "Interupt remapping table size", irta_size);
+    bfdebug_subnhex(0, "Interupt remapping table entry count", irt_entry_count);
+
+    auto irt_base_virt = reinterpret_cast<uintptr_t>(0x104000UL);
+    auto irt_base_phys = reinterpret_cast<uintptr_t>(irta & 0xFFFFFFFFFFFFF000UL);
+    g_cr3->map_4k(irt_base_virt, irt_base_phys);
+    auto irt = gsl::make_span(
+        reinterpret_cast<vtd::irte::value_type *>(irt_base_virt),
+        static_cast<long>(irt_entry_count)
+    );
+
+    uint64_t index = 0;
+    bfdebug_info(0, "-------------------------------------------");
+    bfdebug_info(0, "      VT-d Interrupt Remapping Table");
+    bfdebug_info(0, "-------------------------------------------");
+    for (auto &irte : irt) {
+        if (vtd::irte::p::is_enabled(irte)) {
+            bfdebug_nhex(0, "Interupt Remapping Table Entry At Index:", index);
+            vtd::irte::dump(0, irte);
+        }
+        index++;
+    }
+}
+
+void
+turn_on_interrupt_remapping()
+{
+    bfn::call_once(turn_on_remapping_flag, [&] {
         // If the VT-d intrinsics aren't set up yet, do it here for now
         // (the intrinsics still need to be re-worked so this isn't necessary)
         if(!intel_x64::vtd::iommu::base_addr) {
@@ -56,47 +83,29 @@ interrupt_vector_cpuid(
         }
 
         if (intel_x64::vtd::iommu::gsts_reg::ires::is_enabled()) {
-            bfalert_info(0, "WARNING: Interrupt remapping is already enabled");
-            auto irta = vtd::iommu::irta_reg::get();
-            vtd::iommu::irta_reg::dump(0, irta);
-
-            auto irta_size = (2ul << (vtd::iommu::irta_reg::s::get(irta) + 1));
-            size_t irt_entry_count = irta_size / sizeof(vtd::irte::value_type);
-            bfdebug_subnhex(0, "Interupt remapping table size", irta_size);
-            bfdebug_subnhex(0, "Interupt remapping table entry count", irt_entry_count);
-
-            auto irt_base_virt = reinterpret_cast<uintptr_t>(0x104000UL);
-            auto irt_base_phys = reinterpret_cast<uintptr_t>(irta & 0xFFFFFFFFFFFFF000UL);
-            g_cr3->map_4k(irt_base_virt, irt_base_phys);
-            auto irt = gsl::make_span(
-                reinterpret_cast<vtd::irte::value_type *>(irt_base_virt),
-                static_cast<long>(irt_entry_count)
-            );
-
-            uint64_t index = 0;
-            for (auto &irte : irt) {
-                if (vtd::irte::p::is_enabled(irte)) {
-                    bfdebug_nhex(0, "Interupt Remapping Table Entry At Index:", index);
-                    //
-                    // vtd::irte::dump(0, irte);
-                    // bfdebug_info(0, "");
-                    //
-                    // bfdebug_info(0, "remapping to VISR at interrupt 0x90");
-                    // vtd::irte::v::set(irte, 0x90);
-                }
-                index++;
-            }
-
+            bfalert_info(0, "WARNING: Interrupt remapping is already enabled.");
+            bfalert_info(0, "Dumping remapping tables and configuration without enabling");
         }
         else {
-            // bfdebug_info(0, "Setting up interrupt remapping");
+            bfdebug_info(0, "Enabling VT-d interrupt remapping:");
+            bfdebug_subnhex(0, "Interrupts destined for NDVM IDT vector...", g_ndvm_vector);
+            bfdebug_subnhex(0, "will be redirected to VISR at vector:", g_visr_vector);
+            // -------------------------------------------------------------------------
+            // Set up interrupt remapping table
+            // -------------------------------------------------------------------------
+            bfdebug_info(0, "Setting up interrupt remapping table");
             g_vtd_imap.init();
+            // TODO: Lookup the NIC's programed IRT index, or figure out how to program it
+            // TODO: Figure out the correct destination xAPIC id
+            g_vtd_imap.remap_interrupt(7, g_visr_vector, 1);
+            bfdebug_info(0, "done");
+
             auto gsts_reg_val = ::intel_x64::vtd::iommu::gsts_reg::get();
 
             // -------------------------------------------------------------------------
             // Set the interrupt remapping table pointer
             // -------------------------------------------------------------------------
-            // bfdebug_info(0, "Setting interrupt remapping table pointer...");
+            bfdebug_info(0, "Setting interrupt remapping table pointer...");
             auto irta_value = g_vtd_imap.phys_addr();
             intel_x64::vtd::iommu::irta_reg::eime::disable(irta_value);
             intel_x64::vtd::iommu::irta_reg::s::set(irta_value, 0x9);
@@ -110,15 +119,14 @@ interrupt_vector_cpuid(
             while(1) {
                 volatile auto keep_waiting = intel_x64::vtd::iommu::gsts_reg::irtps::is_disabled();
                 if (!keep_waiting) break;
-                // bfdebug_info(0, "... waiting on interrupt remapping table pointer status bit ...");
+                bfdebug_info(0, "... waiting on interrupt remapping table pointer status bit ...");
             }
-            // bfdebug_info(0, "done");
-            // intel_x64::vtd::iommu::gsts_reg::dump(0);
+            bfdebug_info(0, "done");
 
             // -------------------------------------------------------------------------
             // Enable Interrupt compatibility mode
             // -------------------------------------------------------------------------
-            // bfdebug_info(0, "Enabling interrupt compatibility mode...");
+            bfdebug_info(0, "Enabling interrupt compatibility mode...");
             gsts_reg_val = ::intel_x64::vtd::iommu::gsts_reg::get();
             gsts_reg_val = gsts_reg_val & 0x96FFFFFF;
             intel_x64::vtd::iommu::gcmd_reg::cfi::enable(gsts_reg_val);
@@ -126,15 +134,14 @@ interrupt_vector_cpuid(
             while(1) {
                 volatile auto keep_waiting = intel_x64::vtd::iommu::gsts_reg::cfis::is_disabled();
                 if (!keep_waiting) break;
-                // bfdebug_info(0, "... waiting on interrupt compatibility enable status bit ...");
+                bfdebug_info(0, "... waiting on interrupt compatibility enable status bit ...");
             }
-            // bfdebug_info(0, "done");
-            // intel_x64::vtd::iommu::gsts_reg::dump(0);
+            bfdebug_info(0, "done");
 
             // -------------------------------------------------------------------------
             // Enable Interrupt remapping
             // -------------------------------------------------------------------------
-            // bfdebug_info(0, "Enabling interrupt remapping...");
+            bfdebug_info(0, "Enabling interrupt remapping...");
             gsts_reg_val = ::intel_x64::vtd::iommu::gsts_reg::get();
             gsts_reg_val = gsts_reg_val & 0x96FFFFFF;
             intel_x64::vtd::iommu::gcmd_reg::ire::enable(gsts_reg_val);
@@ -142,23 +149,9 @@ interrupt_vector_cpuid(
             while(1) {
                 volatile auto keep_waiting = intel_x64::vtd::iommu::gsts_reg::ires::is_disabled();
                 if (!keep_waiting) break;
-                // bfdebug_info(0, "... waiting on interrupt remapping enable status bit ...");
+                bfdebug_info(0, "... waiting on interrupt remapping enable status bit ...");
             }
-            // bfdebug_info(0, "done");
-            // intel_x64::vtd::iommu::gsts_reg::dump(0);
-
-            // -------------------------------------------------------------------------
-            // Enable Interrupt compatibility mode again
-            // -------------------------------------------------------------------------
-            // bfdebug_info(0, "Enabling interrupt compatibility mode...");
-            // intel_x64::vtd::iommu::gcmd_reg::cfi::enable();
-            // while(1) {
-            //     volatile auto keep_waiting = intel_x64::vtd::iommu::gsts_reg::cfis::is_disabled();
-            //     if (!keep_waiting) break;
-            //     bfdebug_info(0, "... waiting on interrupt compatibility enable status bit ...");
-            // }
-            // bfdebug_info(0, "done");
-            // intel_x64::vtd::iommu::gsts_reg::dump(0);
+            bfdebug_info(0, "done");
 
             // -------------------------------------------------------------------------
             // Check fault recording to see if we messed anything up
@@ -168,20 +161,55 @@ interrupt_vector_cpuid(
                     printf("\n");
                     bferror_info(0, "Fault occurred during interrupt remapping initialization:");
                     intel_x64::vtd::iommu::frr::dump(0);
-                    printf("\n");
-                    break;
+                    return;
                 }
             }
-
-            // intel_x64::vtd::iommu::gsts_reg::dump(0);
         }
+
+        dump_interrupt_remapping();
     });
+}
+
+bool
+receive_vector_from_ndvm(
+    gsl::not_null<vcpu_t *> vcpu,
+    cpuid_handler::info_t &info
+)
+{
+    bfignored(vcpu);
+    bfignored(info);
+
+    g_ndvm_vector = vcpu->rcx();
+    bfdebug_nhex(0, "Recieved vector from NDVM:", g_ndvm_vector);
+    
+    if(g_visr_vector != 0 && g_ndvm_vector != 0) {
+        turn_on_interrupt_remapping();
+    }
 
     return true;
 }
 
 bool
-interrupt_inject_cpuid(
+receive_vector_from_windows(
+    gsl::not_null<vcpu_t *> vcpu,
+    cpuid_handler::info_t &info
+)
+{
+    bfignored(vcpu);
+    bfignored(info);
+
+    g_visr_vector = vcpu->rcx();
+    bfdebug_nhex(0, "Recieved vector from VISR driver:", g_visr_vector);
+    
+    if(g_visr_vector != 0 && g_ndvm_vector != 0) {
+        turn_on_interrupt_remapping();
+    }
+
+    return true;
+}
+
+bool
+windows_test_interrupt_inject(
     gsl::not_null<vcpu_t *> vcpu,
     cpuid_handler::info_t &info
 )
@@ -191,13 +219,13 @@ interrupt_inject_cpuid(
 
     auto my_vcpu = vcpu_cast(vcpu);
     my_vcpu->queue_external_interrupt(g_visr_vector);
-    // bfdebug_info(0, "interrupt queued for guest");
+    bfdebug_info(0, "VISR requested an interrupt test, injecting to hardware domain now");
 
     return true;
 }
 
 bool
-interrupt_ack_cpuid(
+windows_isr_ack(
     gsl::not_null<vcpu_t *> vcpu,
     cpuid_handler::info_t &info
 )
@@ -205,7 +233,12 @@ interrupt_ack_cpuid(
     bfignored(vcpu);
     bfignored(info);
 
-    // bfdebug_info(0, "The interrupt service routine ran!");
+    bfdebug_info(0, "The VISR interrupt service routine ran!");
+    bfdebug_info(0, "TODO: \"un-remap\" the interrupt by injecting it into the NDVM");
+
+    // TODO: "Un-remap" the interrupt by injecting it into the NDVM
+    // auto ndvm_vcpu = ?;
+    // ndvm_vcpu->queue_external_interrupt(g_ndvm_vector);
 
     return true;
 }
@@ -239,22 +272,38 @@ handle_cfc_out(gsl::not_null<vcpu_t *> vcpu, io_instruction_handler::info_t &inf
     return false;
 }
 
+bfn::once_flag init_flag{};
+
 void
-enable(gsl::not_null<eapis::intel_x64::vcpu *> vcpu)
+enable(
+    gsl::not_null<eapis::intel_x64::vcpu *> vcpu,
+    uint64_t bus,
+    uint64_t device,
+    uint64_t function
+)
 {
+    g_bus = bus;
+    g_device = device;
+    g_function = function;
+
+    vcpu->emulate_cpuid(
+        0xd00dfeed,
+        cpuid_handler::handler_delegate_t::create<receive_vector_from_ndvm>()
+    );
+
     vcpu->emulate_cpuid(
         0xf00dbeef,
-        cpuid_handler::handler_delegate_t::create<interrupt_vector_cpuid>()
+        cpuid_handler::handler_delegate_t::create<receive_vector_from_windows>()
     );
 
     vcpu->emulate_cpuid(
         0xdeadbeef,
-        cpuid_handler::handler_delegate_t::create<interrupt_inject_cpuid>()
+        cpuid_handler::handler_delegate_t::create<windows_test_interrupt_inject>()
     );
 
     vcpu->emulate_cpuid(
         0xcafebabe,
-        cpuid_handler::handler_delegate_t::create<interrupt_ack_cpuid>()
+        cpuid_handler::handler_delegate_t::create<windows_isr_ack>()
     );
 
     vcpu->add_io_instruction_handler(
--- bfvmm/src/hve/arch/intel_x64/vtd/visr_device.cpp
+++ bfvmm/src/hve/arch/intel_x64/vtd/visr_device.cpp
@@ -4,6 +4,8 @@
 
 #include "hve/arch/intel_x64/vtd/vtd_sandbox.h"
 
+#include "hve/arch/intel_x64/vtd/ioapic.h"
+
 namespace vtd_sandbox
 {
 namespace visr_device
@@ -31,9 +33,13 @@ const uint32_t cap_ptr = 0;
 const uint32_t lat_grant_pin_line = 0x100;
 
 // The physical bus/device/function the emulated device will occupy
-const uint64_t g_bus = 3;
-const uint64_t g_device = 0;
-const uint64_t g_function = 0;
+uint64_t g_bus = 0;
+uint64_t g_device = 0;
+uint64_t g_function = 0;
+
+// The two interrupt vectors to be remapped to/from each other
+static volatile uint64_t g_visr_vector = 0;
+static volatile uint64_t g_ndvm_vector = 0;
 
 bool
 handle_cfc_in(
@@ -364,20 +370,138 @@ handle_cff_out(
     return true;
 }
 
-// void
-// enable(gsl::not_null<eapis::intel_x64::vcpu *> vcpu)
 void
-enable(gsl::not_null<eapis::intel_x64::vcpu *> vcpu)
+print_ioapic_redirection_table(gsl::not_null<vcpu_t *> vcpu)
 {
-    // Make sure there isn't a real PCI device at the address we want to emulate
-    uint32_t address = 0x80000000 | g_bus << 16 | g_device << 11 | g_function << 8;
+    bfdebug_info(0, "Looking up IOAPIC Redirection Table");
+    uintptr_t ioapic_base_phys = 0xfec00000;
+    auto my_vcpu = vcpu_cast(vcpu);
+    auto ioapic_base_virt = my_vcpu->map_hpa_4k<uint32_t>(ioapic_base_phys);
+    uint32_t ioregsel = 0;
+    uint32_t iowin = 0;
+    intel_x64::ioapic::rte_t rte = 0;
+
+    ioregsel = 0;
+    ioapic_base_virt.get()[0] = ioregsel;
+    iowin = ioapic_base_virt.get()[4];
+    bfdebug_subnhex(0, "IOAPIC ID:", iowin);
+
+    ioregsel = 1;
+    ioapic_base_virt.get()[0] = ioregsel;
+    iowin = ioapic_base_virt.get()[4];
+    bfdebug_subnhex(0, "IOAPIC Version:", iowin);
+
+    ioregsel = 2;
+    ioapic_base_virt.get()[0] = ioregsel;
+    iowin = ioapic_base_virt.get()[4];
+    bfdebug_subnhex(0, "IOAPIC Arbitration ID:", iowin);
+
+    for(uint32_t i = 0x10; i <= 0x3F; i+=2) {
+        // {31:0]
+        ioregsel = i;
+        ioapic_base_virt.get()[0] = ioregsel;
+        iowin = ioapic_base_virt.get()[4];
+        rte = iowin;
+
+        // {63:32]
+        ioregsel = i;
+        ioapic_base_virt.get()[0] = ioregsel;
+        iowin = ioapic_base_virt.get()[4];
+        rte = rte | (iowin << 32);
+        if(intel_x64::ioapic::rte::vector::get(rte) != 0xFF && rte != 0) {
+            bfdebug_nhex(0, "IOAPIC Redirection Table Entry index:", i - 0x10);
+            intel_x64::ioapic::rte::dump(0, rte);
+            bfdebug_info(0, "");
+        }
+    }
+}
+
+bool
+receive_vector_from_ndvm(
+    gsl::not_null<vcpu_t *> vcpu,
+    cpuid_handler::info_t &info
+)
+{
+    bfignored(vcpu);
+    bfignored(info);
+
+    g_ndvm_vector = vcpu->rcx();
+    bfdebug_nhex(0, "Recieved vector from NDVM:", g_ndvm_vector);
+
+    return true;
+}
+
+bool
+receive_vector_from_windows(
+    gsl::not_null<vcpu_t *> vcpu,
+    cpuid_handler::info_t &info
+)
+{
+    bfignored(vcpu);
+    bfignored(info);
+
+    g_visr_vector = vcpu->rcx();
+    bfdebug_nhex(0, "Recieved vector from VISR driver:", g_visr_vector);
+    print_ioapic_redirection_table(vcpu);
+
+    return true;
+}
+
+bool
+windows_test_interrupt_inject(
+    gsl::not_null<vcpu_t *> vcpu,
+    cpuid_handler::info_t &info
+)
+{
+    bfignored(vcpu);
+    bfignored(info);
+
+    auto my_vcpu = vcpu_cast(vcpu);
+    my_vcpu->queue_external_interrupt(g_visr_vector);
+    bfdebug_info(0, "VISR driver requested an interrupt test, injecting to HDVM now");
+
+    return true;
+}
+
+bool
+windows_isr_ack(
+    gsl::not_null<vcpu_t *> vcpu,
+    cpuid_handler::info_t &info
+)
+{
+    bfignored(vcpu);
+    bfignored(info);
+
+    bfdebug_info(0, "The VISR ran!");
+
+    // TODO: Inject the interrupt into the NDVM
+    // auto ndvm_vcpu = ?;
+    // ndvm_vcpu->queue_external_interrupt(g_ndvm_vector);
+
+    return true;
+}
+
+void
+enable(
+    gsl::not_null<eapis::intel_x64::vcpu *> vcpu,
+    uint64_t bus,
+    uint64_t device,
+    uint64_t function
+)
+{
+    // Make sure there is a real PCI device at the address we want to emulate
+    uint32_t address = 0x80000000 | bus << 16 | device << 11 | function << 8;
     ::x64::portio::outd(0xCF8, address);
     auto data = ::x64::portio::ind(0xCFC);
-    if(data != 0xFFFFFFFF) {
-        bferror_info(0, "Failed to initalize Bareflank virtual PCI device,");
-        bferror_nhex(0, "a real PCI device exists at address:", address);
+    if(data == 0xFFFFFFFF) {
+        bferror_info(0, "Failed to initalize Bareflank VISR device,");
+        bferror_nhex(0, "A real PCI device must exist at IO address:", address);
         return;
     }
+
+    g_bus = bus;
+    g_device = device;
+    g_function = function;
 
     g_virtual_pci_config = gsl::make_span(
         static_cast<uint32_t *>(alloc_page()),
@@ -405,7 +529,9 @@ enable(gsl::not_null<eapis::intel_x64::vcpu *> vcpu)
     g_virtual_pci_config.at(14) = 0;
     g_virtual_pci_config.at(15) = lat_grant_pin_line;
 
-    // Initialize handlers
+    // -------------------------------------------------------------------------
+    // PCI configuration space handlers
+    // -------------------------------------------------------------------------
     vcpu->add_io_instruction_handler(
         0xCFC,
         io_instruction_handler::handler_delegate_t::create <handle_cfc_in>(),
@@ -428,6 +554,29 @@ enable(gsl::not_null<eapis::intel_x64::vcpu *> vcpu)
         0xCFF,
         io_instruction_handler::handler_delegate_t::create <handle_cff_in>(),
         io_instruction_handler::handler_delegate_t::create <handle_cff_out>()
+    );
+
+    // -------------------------------------------------------------------------
+    // Handlers to coordinate interupt injection
+    // -------------------------------------------------------------------------
+    vcpu->emulate_cpuid(
+        0xd00dfeed,
+        cpuid_handler::handler_delegate_t::create<receive_vector_from_ndvm>()
+    );
+
+    vcpu->emulate_cpuid(
+        0xf00dbeef,
+        cpuid_handler::handler_delegate_t::create<receive_vector_from_windows>()
+    );
+
+    vcpu->emulate_cpuid(
+        0xdeadbeef,
+        cpuid_handler::handler_delegate_t::create<windows_test_interrupt_inject>()
+    );
+
+    vcpu->emulate_cpuid(
+        0xcafebabe,
+        cpuid_handler::handler_delegate_t::create<windows_isr_ack>()
     );
 
     // bfdebug_info(0, "Virtual interrupt service route device initialized");
